2828f1fb18a6eaa90a902df30f0fa9b7fb326c0e
shunsuke-hoshino <hosino0625@gmail.com>
2022-10-10 15:27:02 +0900

    cash削除

--- search/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class SearchForm(forms.Form):
-    searchname = forms.CharField(
-        max_length = 100,
-        required = True,
-    )
--- search/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Word(models.Model):
-    word = models.CharField(max_length = 100)
